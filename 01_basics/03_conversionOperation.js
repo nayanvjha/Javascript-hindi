8b0a7d657874d9c365ec99a5849f8bf2f14ca149
nayanvjha <145110888+nayanvjha@users.noreply.github.com>
2024-04-13 19:31:01 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = true
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// whenever u do conversion of a number then it easily gets converted into a numbe
+
+// "33" => 33
+
+// "33abc" => NaN(NOT A NUMBER)
+     //(conversion)
+// TRUE => 1 ; False => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+
+// ""(empty string)=> false
+
+// "nayan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
