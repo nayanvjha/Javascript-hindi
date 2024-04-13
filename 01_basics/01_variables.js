2f9793660d6f26fb538c1c66f81901f685ba1fc6
nayanvjha <145110888+nayanvjha@users.noreply.github.com>
2024-04-13 13:15:46 +0000

    dataypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+
+let accountEmail = "nayanvjha@gmail.com"
+
+var accountPassword = "123456"
+
+accountCity = "Jaipur"
+
+accountEmail = "hc@hc.com"
+
+accountPassword = "21212121"
+
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+
+*/
+
+console.table([accountId, accountEmail, accountPassword])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+3 ) // we are using nodejs, not browser
+
+console.log(3+3)
+
+ console.log("Nayan")
+
+ let name = "hitesh"
+ let age = 18
+ let isLoggedIn = false
+ let state;
+
+ //data types
+
+ // number => 2 to power 53
+
+ //bigint
+ //string =>""
+ //boolean => true/false
+ //null(is an object type) => standalone value ..wo undefined nhi h khali hai..ex if we ask a server to send temp if it sends temp 0 coz there is a problem in server then in that case we won't prefer 0 we
+ //will prefer null
+
+// undefined => ex let state is undefined
+
+//6) symbol => unique
+
+//7) object
+
+console.log(typeof "nayan");
