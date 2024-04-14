695cbc7eac7c48709e2a07236901ce3665f10ee2
nayanvjha <145110888+nayanvjha@users.noreply.github.com>
2024-04-14 07:28:10 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+console.log(null > 0);  // these type of conversions are always confusing where both the data types in comparison are not same
+
+console.log(null == 0);
+
+console.log(null >= 0);
+
+
+// === it strictly checks i.e it checks both the values and its data type
