f033e02485a70bfb0656cf50d4321f044c4a4f42
nayanvjha <145110888+nayanvjha@users.noreply.github.com>
2024-04-29 21:04:02 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,39 @@
+const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+// // console.log(hundreds.toLocaleString());  //a/q to us standards
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++ Maths ++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));// abs - absolute value
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+
+console.log(Math.random());  // IT GIVES VALUE B/W O AND 1
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);// it will round off and give exact values between 1 and 10
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min +1))+min);// + min means at least min >= 10 it will give for sure 
+   // its a standard formula 
